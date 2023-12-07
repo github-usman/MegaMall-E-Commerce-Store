72dd25c4870f5703b5ec411c5f1c03808e2ccdda
github-usman <usmantoyou@gmail.com>
2023-12-07 11:28:33 +0530

    Frontend : routing, login,signup page complete

---
+++ frontend/src/pages/SignupPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Signup } from '../features/auth/component/Signup'
+
+const SignupPage = () => {
+  return (
+    <div><Signup/></div>
+  )
+}
+
+export default SignupPage
